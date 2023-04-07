5e0e3c79ee2339ff8751c6171744703b93d2014b
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-04-07 21:06:39 +0200

    Added the numbers highlighting to the report addition form

--- src/components/ReportForm.tsx
+++ src/components/ReportForm.tsx
@@ -6,25 +6,24 @@ type props = {
 const ReportForm = (props : props) => {
 
 
-    const [selectedInput , setSelectedInput]  = useState(1);
-    const modifyCurrentInput = (currentInput : number) =>{
-        props.setCurrentInputFunction(se);
-        setSelectedInput(currentInput)
-      }
-
     return ( 
         <div>
             <div className="text-[#22543D] font-bold text-xl text-center w-[100%] "><h2>REPORT SIGHTING</h2></div>
             <div className="flex flex-col w-[100%] justify-center items-center">
                 <div className="p-5 flex flex-col bg-slate-200 rounded-md w-[85%] mt-8">
                     <label className="text-md font-bold text-[#22543d]" htmlFor="Current Location:">CURRENT LOCATION:</label>
-                    <input className="text-sm p-2 rounded-md mt-2 shadow-md" type="text" name="" id="" placeholder="Current Location" />
+                    <input className="text-sm p-2 rounded-md mt-2 shadow-md" type="text" name="" id="" placeholder="Current Location" value={"Akagera National Park"}  readOnly onFocus={()=> {
+                        props
+                        .setCurrentInputFunction(1)
+                    }}/>
                     </div>
 
                     <div className="p-5 flex flex-col bg-slate-200 rounded-md w-[85%] mt-8">
                     <label className="text-md font-bold text-[#22543d]" htmlFor="Add Sighting:">ADD SIGHTING:</label>
-                    {/* <input className="text-sm p-2 rounded-md mt-2 shadow-md" type="text" name="" id="" placeholder="Select Animal" /> */}
-                    <select name="" className="text-sm p-2 rounded-md mt-2 shadow-md" id="">
+                    <select name="" className="text-sm p-2 rounded-md mt-2 shadow-md" id="" onFocus={()=> {
+                        props
+                        .setCurrentInputFunction(2)
+                    }}>
                     <option value="">Lion</option>
                     <option value="">Buffalo</option>
                     <option value="">Cheetah</option>
@@ -36,8 +35,10 @@ const ReportForm = (props : props) => {
 
                     <div className="p-5 flex flex-col bg-slate-200 rounded-md w-[85%] mt-8">
                     <label className="text-md font-bold text-[#22543d]" htmlFor="Time:">TIME:</label>
-                    {/* <input  type="text" name="" id="" placeholder="Select Time" /> */}
-                    <select name="" className="text-sm p-2 rounded-md mt-2 shadow-md" id="">
+                    <select name="" className="text-sm p-2 rounded-md mt-2 shadow-md" id="" onFocus={()=> {
+                        props
+                        .setCurrentInputFunction(3)
+                    }}>
                         <option value="">1 minute ago</option>
                         <option value="">2 minute ago</option>
                         <option value="">3 minute ago</option>
