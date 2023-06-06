a26de449719352a2de25d2d89a26e3612190186c
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-06-06 04:18:34 +0200

    Added the admin header to the dashboard

--- src/components/AdminHeader.tsx
+++ src/components/AdminHeader.tsx
@@ -3,12 +3,12 @@ import {AiOutlineSearch} from 'react-icons/ai'
 
 const Header = () => {
     return ( 
-        <div className="bg-white w-[100vw] flex item-center justify-center ">
-            <div className="w-[92%] flex flex-row items-center border-b-2 border-gray-300 justify-between">
-                <div>LOGO</div>
+        <div className=" w-[100vw] h-[100%] flex item-center justify-center ">
+            <div className="w-[92%] flex flex-row font-bold text-xl items-center border-b-2 border-gray-300 justify-between">
+                <div>Welcome Back, Admin AD23425</div>
                 <div className="flex ">
-                <div className='h-[100%] p-2 flex justify-center items-center text-3xl text-gray-400 font-light'><AiOutlineSearch /></div>
-                    <div className='h-[100%] p-2 flex justify-center items-center text-3xl text-gray-400 font-extralight'><BiUserCircle /></div>
+                <div className='h-[100%] p-2 flex justify-center items-center text-2xl text-gray-400 font-light'><AiOutlineSearch /></div>
+                    <div className='h-[100%] p-2 flex justify-center items-center text-2xl text-gray-400 font-extralight'><BiUserCircle /></div>
                 </div>
             </div>
         </div>
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,5 @@
+import AdminHeader from "../components/AdminHeader";
+
 import SideBarDash from "../components/SideBarDash";
 
 const Dashboard = () => {
@@ -6,7 +8,12 @@ const Dashboard = () => {
             <div className="w-[20%] h-[100%] flex flex-col bg-white justify-center items-center">
                 <SideBarDash />
             </div>
-            <div className="w-[80%] h-[100%] flex flex-col bg-[#F5F5F5] justify-center items-center">Hello World</div>
+            <div className="w-[80%] h-[100%] flex flex-col bg-[#F5F5F5] justify-center items-center">
+                <div className="w-[100%] h-[10%]  flex flex-row justify-center items-center">
+                    <AdminHeader />
+                </div>
+                <div className="w-[100%] h-[90%] bg-blue-300 flex flex-row justify-center items-center">hello World</div>
+            </div>
         </div>
      );
 }
