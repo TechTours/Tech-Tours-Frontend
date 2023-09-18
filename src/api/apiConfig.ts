2334a40c28a98faa60a4eeb804549d7f3961385f
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-09-18 16:05:25 +0200

    Added the realtime communication between the server and the activity api and the frontend

--- src/api/apiConfig.ts
+++ src/api/apiConfig.ts
@@ -1,6 +1,8 @@
 // apiConfig.ts
 
 export const BASE_URL = 'http://194.163.167.131:8020';
+
+export const BASE_SOCKET_URL = 'ws://194.163.167.131:8020'
 // export const BASE_URL = 'http://192.168.0.198:3000';
 export const ADMIN_KEY = 'rcaEIRGNUIWERBGUIWRBFREUGFER';
 // Add more configuration settings as needed
--- src/components/Maps/MapComponent.tsx
+++ src/components/Maps/MapComponent.tsx
@@ -1,43 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import lion from '../../images/lion.png';
 import buffalo from '../../images/buffalo.png';
 import elephant from '../../images/elephant.png';
+import leopard from '../../images/leopard.png'
+import rhino from '../../images/rhinoceros.png'
+import io from 'socket.io-client'; // Import socket.io-client
+import { BASE_SOCKET_URL, BASE_URL } from '../../api/apiConfig';
+import axios from 'axios';
 
-interface MapComponentState {
-  pins: { lat: number; lng: number; title: string; type: string }[];
-}
+const MapComponent: React.FC = () => {
+  const mapContainer = useRef<HTMLDivElement | null>(null);
+  const [pins, setPins] = useState<any[]>([]); // Use state to store pins
 
-class MapComponent extends React.Component<{}, MapComponentState> {
-  private mapContainer: React.RefObject<HTMLDivElement>;
-  private map: mapboxgl.Map | undefined;
-
-  constructor(props: {}) {
-    super(props);
-
-    this.mapContainer = React.createRef();
-
-    this.state = {
-      pins: [
-        { lat: -1.821117070534592, lng: 30.706783777721494, title: 'Akagera', type: 'type1' },
-        { lat: -1.821117070534592, lng: 30.706783777721494, title: 'Akagera'  , type: 'type1'  },
-        { lat: -1.8145539869623148, lng: 30.710066971782197, title: 'Akagera' , type : 'type2' },
-        { lat: -1.9020597758447721, lng: 30.69583979751915, title: 'Akagera' , type : 'type3' },
-        { lat: -1.9294044343094023, lng: 30.70120671841402, title: 'Akagera' , type : 'type1' },
-        {lat: -1.7213614991712078 , lng : 30.696347817252708 , title: 'Akagera' , type : 'type2' },
-        {lat: -1.7513136776170033 , lng : 30.708573964829238 , title: 'Akagera' , type : 'type3' },
-        {lat: -1.825567686106359 , lng : 30.71863409622777 , title: 'Akagera' , type : 'type1' },
-        {lat: -1.843656 , lng : 30.732354 , title: 'Akagera' , type : 'type2' },
-      ],
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     mapboxgl.accessToken = 'pk.eyJ1IjoiaXphYmF5bzciLCJhIjoiY2xreWdubDZuMGlyaDNzc2l0dW1tOGo4NCJ9.dMAnMhcPZJSTlZKniAu7ag';
 
-    this.map = new mapboxgl.Map({
-      container: this.mapContainer.current!,
+    const map = new mapboxgl.Map({
+      container: mapContainer.current!,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [30.70120671841402, -1.9294044343094023],
       zoom: 10,
@@ -47,19 +28,88 @@ class MapComponent extends React.Component<{}, MapComponentState> {
       type1: lion,
       type2: buffalo,
       type3: elephant,
+      type4: leopard,
+      type5: rhino
       // Add more type-to-icon mappings as needed
     };
 
-    this.state.pins.forEach((pin, index) => {
-      const marker = new mapboxgl.Marker({
-        element: this.createCustomMarker(pin.type, iconMappings[pin.type], pin),
-      })
-        .setLngLat([pin.lng, pin.lat])
-        .addTo(this.map!);
-    });
-  }
+    // Authentication headers
+    const authToken = localStorage.getItem("token"); // Replace with your actual authentication token
+    const authHeaders = {
+      Authorization: `Bearer ${authToken}`,
+    };
 
-  createCustomMarker(type: string, iconUrl: string, pin: any): HTMLDivElement {
+    // Initialize WebSocket connection with authentication headers
+    const socket = io(`${BASE_SOCKET_URL}`, {
+      extraHeaders: authHeaders,
+    });
+
+    // Listen for WebSocket errors
+    socket.on('connect_error', (error) => {
+      console.error('WebSocket connection error:', error);
+    });
+
+    // Listen for WebSocket updates
+    socket.on('data', (data) => {
+      // Process and update pins based on the received data
+      const updatedPins = data.map((animal: any) => ({
+        lat: animal.latitude,
+        lng: animal.longitude,
+        title: animal.animal,
+        type: animal.animal == 'Lion' ? 'type1' : animal.animal == 'Buffalo' ? 'type2' : animal.animal == 'Elephant' ? 'type3' : animal.animal == 'Leopard' ? 'type4' : 'type5',
+      }));
+      setPins(updatedPins);
+    
+      // Clear existing markers on the map
+      map.getSource('animal-locations').setData({
+        type: 'FeatureCollection',
+        features: [],
+      });
+    
+      // Add markers to the map based on updatedPins
+      updatedPins.forEach((pin : any, index : any) => {
+        const marker = new mapboxgl.Marker({
+          element: createCustomMarker(pin.type, iconMappings[pin.type], pin),
+        })
+          .setLngLat([pin.lng, pin.lat])
+          .addTo(map);
+      });
+    });
+    
+    // Create a GeoJSON source for markers
+    map.on('load', () => {
+      map.addSource('animal-locations', {
+        type: 'geojson',
+        data: {
+          type: 'FeatureCollection',
+          features: [],
+        },
+      });
+    
+      // Add a layer to display markers
+      map.addLayer({
+        id: 'animal-locations',
+        type: 'symbol',
+        source: 'animal-locations',
+        layout: {
+          'icon-image': ['get', 'icon'],
+          'icon-size': 0.5, // Adjust the size of the icons
+          'icon-allow-overlap': true, // Allow icons to overlap
+        },
+      });
+
+      // Your code to update markers should go here
+    });
+
+    // Cleanup on unmount
+    return () => {
+      map.remove(); // Remove the map instance to prevent memory leaks
+      socket.disconnect(); // Disconnect WebSocket on unmount
+    };
+  }, []);
+
+  const createCustomMarker = (type: string, iconUrl: string, pin: any) => {
+    // ... (same as your previous code)
     const markerElement = document.createElement('div');
     markerElement.className = 'flex items-center gap-2 p-1 rounded-md w-fit text-sm bg-white/60'; // Adjust size here
   
@@ -87,13 +137,9 @@ class MapComponent extends React.Component<{}, MapComponentState> {
     markerElement.appendChild(textContainer);
   
     return markerElement;
-  }
-  
-  render() {
-    return (
-      <div ref={this.mapContainer} className='w-full h-screen'></div>
-    );
-  }
-}
+  };
+
+  return <div ref={mapContainer} className='w-full h-screen'></div>;
+};
 
 export default MapComponent;
